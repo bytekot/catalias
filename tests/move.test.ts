fc414d1b883630c53d83f3ba84667aedc0e1b0f1
Sergey Kot <bytekot@gmail.com>
2021-01-24 10:33:00 +0300

    Add test for Move

---
+++ tests/move.test.ts
@@ -0,0 +1,12 @@
+import { Move } from '../src/ts/core';
+
+test('End move after waiting', () => {
+    const moveDurationMilliseconds = 60000;
+    const startMilliseconds = Date.now();
+    const move = new Move(moveDurationMilliseconds);
+
+    move.start(startMilliseconds);
+    move.tick(startMilliseconds + moveDurationMilliseconds);
+
+    expect(move.isFinished()).toBe(true);
+});
