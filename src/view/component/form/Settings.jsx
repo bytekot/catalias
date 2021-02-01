755a432df86e716928babe3a1e52b3275a1ec6b7
Sergey Kot <bytekot@gmail.com>
2021-02-01 03:01:40 +0300

    Refact settings form (divide into components)

--- src/view/component/form/Settings.jsx
+++ src/view/component/form/Settings.jsx
@@ -2,6 +2,52 @@ import React from 'react';
 import TextField from '../field/Text.jsx';
 import CheckboxField from '../field/Checkbox.jsx';
 
+const TeamSet = ({ teamName1, teamName2, updateHandler }) => (
+    <fieldset>
+        <legend>Команды</legend>
+        <TextField
+            name="teamName1"
+            label="Команда 1"
+            defaultValue={teamName1}
+            onBlur={updateHandler}
+        />
+        <TextField
+            name="teamName2"
+            label="Команда 2"
+            defaultValue={teamName2}
+            onBlur={updateHandler}
+        />
+    </fieldset>
+);
+
+const DictionarySet = () => (
+    <fieldset>
+        <legend>Наборы слов</legend>
+        <CheckboxField
+            name="words"
+            label="Базовый набор"
+        />
+    </fieldset>
+);
+
+const SettingSet = ({ moveDuration, scoreToWin, updateHandler }) => (
+    <fieldset>
+        <legend>Настройки</legend>
+        <TextField
+            name="moveDuration"
+            label="Продолжительность хода (сек.)"
+            defaultValue={moveDuration}
+            onBlur={updateHandler}
+        />
+        <TextField
+            name="scoreToWin"
+            label="Очков для победы"
+            defaultValue={scoreToWin}
+            onBlur={updateHandler}
+        />
+    </fieldset>
+);
+
 export default class SettingsForm extends React.Component {
     state = {
         teamName1: 'Бешеные псы',
@@ -16,6 +62,7 @@ export default class SettingsForm extends React.Component {
         });
     }
 
+    // todo: team names
     onNewGameButtonClick = event => {
         const state = this.state;
 
@@ -28,47 +75,18 @@ export default class SettingsForm extends React.Component {
 
     render() {
         return (
-            <div name="settings-container">
-                <fieldset>
-                    <legend>Команды</legend>
-                    <TextField
-                        name="teamName1"
-                        label="Команда 1"
-                        defaultValue={this.state.teamName1}
-                        onBlur={this.updateState}
-                    />
-                    <TextField
-                        name="teamName2"
-                        label="Команда 2"
-                        defaultValue={this.state.teamName2}
-                        onBlur={this.updateState}
-                    />
-                </fieldset>
-
-                <fieldset>
-                    <legend>Наборы слов</legend>
-                    <CheckboxField
-                        name="words"
-                        label="Базовый набор"
-                    />
-                </fieldset>
-
-                <fieldset>
-                    <legend>Настройки</legend>
-                    <TextField
-                        name="moveDuration"
-                        label="Продолжительность хода (сек.)"
-                        defaultValue={this.state.moveDuration}
-                        onBlur={this.updateState}
-                    />
-                    <TextField
-                        name="scoreToWin"
-                        label="Очков для победы"
-                        defaultValue={this.state.scoreToWin}
-                        onBlur={this.updateState}
-                    />
-                </fieldset>
-
+            <div>
+                <TeamSet
+                    teamName1={this.state.teamName1}
+                    teamName2={this.state.teamName2}
+                    updateHandler={this.updateState}
+                />
+                <DictionarySet />
+                <SettingSet 
+                    moveDuration={this.state.moveDuration}
+                    scoreToWin={this.state.scoreToWin}
+                    updateHandler={this.updateState}
+                />
                 <div className="button-container">
                     <button name="button-start-game" onClick={this.onNewGameButtonClick}>Играть</button>
                 </div>
