4ad8dfcb8bf7e6ae6e8ad7b77ccd1bcbda766dc4
Sergey Kot <bytekot@gmail.com>
2021-02-01 03:00:47 +0300

    Refact app component for single rendering

--- src/view/view.jsx
+++ src/view/view.jsx
@@ -10,41 +10,53 @@ import { Dictionary } from '../core/Dictionary';
 
 class App extends React.Component {
 
+    state = {};
+
     onNewMoveButtonClick = () => {
-        const game = this.props.game;
-        const move = new Move(game.moveDuration * 1000);
-        
-        ReactDOM.render(
-            <MoveForm
-                duration={game.moveDuration}
-                getWord={game.dictionary.getRandomWord.bind(game.dictionary)}
-                move={move}
-            />,
-            document.getElementById('root')
-        );
+        this.setState({
+            move: new Move(this.state.game.moveDuration * 1000)
+        });
     }
 
     onNewGameButtonClick = ({ teamNames, moveDuration, scoreToWin }) => {
         const teams = teamNames.map(name => new Team(name));
         const dictionary = new Dictionary();
-
-        this.props.game = new Game(teams, dictionary, {
+        const game = new Game(teams, dictionary, {
             moveDuration: moveDuration,
             scoreToWin: scoreToWin
         });
 
-        ReactDOM.render(
-            <GameForm
-                teamNames={teamNames}
-                scoreToWin={scoreToWin}
-                onButtonClick={this.onNewMoveButtonClick}
-            />,
-            document.getElementById('root')
-        );
+        this.setState({
+            teamNames: teamNames,
+            moveDuration: moveDuration,
+            scoreToWin: scoreToWin,
+            game: game
+        });
     }
 
     render() {
-        return <SettingsForm onButtonClick={this.onNewGameButtonClick} />;
+        const state = this.state;
+
+        if (state.move) {
+            return (
+                <MoveForm
+                    duration={state.moveDuration}
+                    getWord={state.game.dictionary.getRandomWord.bind(state.game.dictionary)}
+                    move={state.move}
+                />
+            )
+        }
+
+        return (
+            !state.game
+                ? <SettingsForm onButtonClick={this.onNewGameButtonClick} />
+                : <GameForm
+                    teamNames={state.teamNames}
+                    scoreToWin={state.scoreToWin}
+                    onButtonClick={this.onNewMoveButtonClick}
+                    hidden={state.hidden1}
+                />
+        );
     }
 }
 
