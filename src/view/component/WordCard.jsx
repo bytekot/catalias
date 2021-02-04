792ce659a3353847cbb6ad7a698ed502bf2291b7
Sergey Kot <bytekot@gmail.com>
2021-02-05 00:19:46 +0300

    Fix showing dictionary during animation

--- src/view/component/WordCard.jsx
+++ src/view/component/WordCard.jsx
@@ -3,29 +3,35 @@ import React, { useState, useEffect, useRef } from 'react';
 const WordCard = ({ word, dictionary }) => {
     const [animated, setAnimated] = useState(false);
     const previousWordReference = useRef();
-    const previousWord = previousWordReference.current;
+    const previousWordData = previousWordReference.current;
 
     useEffect(() => {
-        if (typeof previousWord !== 'undefined' && previousWord !== word) {
+        if (typeof previousWordData !== 'undefined' && previousWordData.word !== word) {
             setAnimated(true);
 
             const timerId = setTimeout(() => {
-                previousWordReference.current = word;
+                previousWordReference.current = {
+                    word: word,
+                    dictionary: dictionary
+                };
                 setAnimated(false);
             }, 400);
 
             return () => clearTimeout(timerId);
         }
 
-        previousWordReference.current = word;
+        previousWordReference.current = {
+            word: word,
+            dictionary: dictionary
+        };
     }, [word]);
 
     return (
         <div className="card-container">
             <div className={`card ${animated ? 'animated' : ''}`}>
                 <span className="move-word">
-                    <span>{!animated ? word : previousWord}</span>
-                    <p className="sub-label">({dictionary})</p>
+                    <span>{!animated ? word : previousWordData.word}</span>
+                    <p className="sub-label">({!animated ? dictionary : previousWordData.dictionary})</p>
                 </span>
             </div>
             <div className="card"></div>
